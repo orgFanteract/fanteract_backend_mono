bb7b22b9c909f6423f3199ecacb6f7718c2d47ba
추호성 <hosung@chuhoseong-ui-MacBookPro.local>
2025-12-03 11:29:58 +0900

    FEAT

--- src/main/kotlin/org/fanteract/config/SwaggerConfig.kt
+++ src/main/kotlin/org/fanteract/config/SwaggerConfig.kt
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Configuration
 @OpenAPIDefinition(
     servers = [
         Server(url = "http://localhost:8080", description = "로컬 서버"),
+        Server(url = "http://3.38.43.146:8080", description = "외부 서버")
     ]
 )
 @Configuration
