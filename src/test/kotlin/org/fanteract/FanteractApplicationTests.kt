640caaec45371df227d1786e0ba898cf84fcf344
추호성 <hosung@chuhoseong-ui-MacBookPro.local>
2025-12-04 01:06:32 +0900

    FEAT: renew cicd file

--- src/test/kotlin/org/fanteract/FanteractApplicationTests.kt
+++ src/test/kotlin/org/fanteract/FanteractApplicationTests.kt
@@ -2,10 +2,11 @@ package org.fanteract
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FanteractApplicationTests {
-
 	@Test
 	fun contextLoads() {
 	}
